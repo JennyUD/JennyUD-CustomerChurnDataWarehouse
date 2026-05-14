74f42114274dc4800cd211ce00d16b5c1e2c5262
JennyUD <igboanugouj9599@gmail.com>
2026-05-13 20:14:07 -0400

    checked gender conflict

--- etl/__01extract_kaggle.py
+++ etl/__01extract_kaggle.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 path = r"data\processed\clean_churn.csv"
 df = pd.read_csv(path)
+print(f"Number of rows in df: {len(df)}")
 
 def get_schema(df):
     schema = pd.DataFrame({
@@ -13,6 +14,6 @@ def get_schema(df):
     print(schema)
     return schema
   
-get_schema(df)
+#get_schema(df)
 
 
